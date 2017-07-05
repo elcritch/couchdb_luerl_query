d4a489578cfe88fc55d36fd55b442ea907b1bd6c
Jaremy Creechley <creechley@gmail.com>
2017-07-04 23:31:26 -0600

    Setting up basic structure.

---
+++ lib/couchdb_luerl_query.ex
@@ -0,0 +1,18 @@
+defmodule CouchdbLuerlQuery do
+  @moduledoc """
+  Documentation for CouchdbLuerlQuery.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CouchdbLuerlQuery.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/couchdb_luerl_query_test.exs
@@ -0,0 +1,8 @@
+defmodule CouchdbLuerlQueryTest do
+  use ExUnit.Case
+  doctest CouchdbLuerlQuery
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
